34261dff94d1051a627784e350d69eca11bfdf21
Onsraa <truongteddy306@icloud.com>
2024-11-04 23:14:02 +0100

    add paddles, delete Position component and more logs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 use bevy::{
     core::FrameCount,
-    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     prelude::*,
     window::{PresentMode, WindowTheme},
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
 };
+
+use iyes_perf_ui::prelude::*;
 
 const RES: (f32, f32) = (500.0, 500.0);
 const MAX_ROUND: u8 = 5;
+const PADDLE_WIDTH: f32 = 40.0;
+const PADDLE_HEIGHT: f32 = 5.0;
+const PADDLE_COLOR: Color = Color::srgb(0.0,0.7,0.0);
+
 #[derive(Component)]
 struct Player(u8);
 
-#[derive(Debug)]
-#[derive(Component)]
-struct Position {
-    x: f32,
-    y: f32,
-}
 #[derive(Component)]
 struct Score(u32);
 
@@ -43,10 +43,15 @@ fn main() {
                 }),
                 ..default()
             }),
-            LogDiagnosticsPlugin::default(),
-            FrameTimeDiagnosticsPlugin,
             GraphicsPlugin,
         ))
+
+        .add_plugins(bevy::diagnostic::LogDiagnosticsPlugin::default())
+        .add_plugins(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
+        .add_plugins(bevy::diagnostic::EntityCountDiagnosticsPlugin)
+        .add_plugins(bevy::diagnostic::SystemInformationDiagnosticsPlugin)
+        //.add_plugins(PerfUiPlugin)
+        
         .add_systems(Startup, (startup_system, check_startup).chain())
         .run();
 }
@@ -55,6 +60,7 @@ struct GraphicsPlugin;
 
 impl Plugin for GraphicsPlugin {
     fn build(&self, app: &mut App) {
+        app.insert_resource(ClearColor(Color::srgb(0.0,0.0,0.0)));
         app.add_systems(Update, (make_visible, toggle_vsync));
     }
 }
@@ -78,18 +84,42 @@ fn toggle_vsync(input: Res<ButtonInput<KeyCode>>, mut windows: Query<&mut Window
     }
 }
 
-fn startup_system(mut commands: Commands){
+fn startup_system(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>){
     commands.spawn(Camera2dBundle::default());
+    commands.spawn(PerfUiCompleteBundle::default());
+    
+    
+    let shape: Mesh2dHandle = Mesh2dHandle(meshes.add(Rectangle::new(PADDLE_HEIGHT, PADDLE_WIDTH)));
     commands.insert_resource(GameState { current_round: 0, max_rounds: MAX_ROUND });
-    commands.spawn((Player(1), Position {x: RES.0 / 2.0, y: 100.0}, Score(0)));
-    commands.spawn((Player(2), Position {x: RES.0 / 2.0, y: RES.1 - 100.0}, Score(0)));
+    commands.spawn((
+        Player(1),
+        Score(0),
+        MaterialMesh2dBundle {
+            mesh: shape,
+            material: materials.add(PADDLE_COLOR),
+            transform: Transform::from_xyz(- (RES.1/2.0 - 50.0),  0.0, 0.0),
+            ..default()
+        })
+    );
+
+    let shape: Mesh2dHandle = Mesh2dHandle(meshes.add(Rectangle::new(PADDLE_HEIGHT, PADDLE_WIDTH)));
+    commands.spawn((
+        Player(2),
+        Score(0),
+        MaterialMesh2dBundle {
+            mesh: shape,
+            material: materials.add(PADDLE_COLOR),
+            transform: Transform::from_xyz(RES.1 / 2.0 - 50.0, 0.0, 0.0),
+            ..default()
+        },
+    ));
 }
 
-fn check_startup(query: Query<(&Player, &Position, &Score)>, game_state: Res<GameState>){
+fn check_startup(query: Query<(&Player, &Transform, &Score)>, game_state: Res<GameState>){
     println!("-------------------------------------------------------------");
     println!("Game state => Current round : {} | Max rounds : {}", game_state.current_round, game_state.max_rounds);
-    for (player, position, score) in &query {
-        println!("Player : {} | Position : {:?} | {}", player.0, position, score.0);
+    for (player, transform, score) in &query {
+        println!("Player : {} | Position : {:?} | {}", player.0, transform, score.0);
     }
     println!("-------------------------------------------------------------");
 }
